067437a8e9ae6af9b3f2f16f4c56f7398f7b0dc0
Reeshabh <reeshabhkumarranjan@gmail.com>
2019-11-14 17:06:43 +0530

    Modified preprocessing code.

--- Problem 1/MLP.py
+++ Problem 1/MLP.py
@@ -37,7 +37,8 @@ class NeuralNet:
 
 		# initialise the weights
 		for layer in range(num_layers - 1):
-			self.weights[layer] = 0.01 * np.random.normal(loc=0, scale=1, size=(self.num_nodes[layer], self.num_nodes[layer + 1]))
+			self.weights[layer] = 0.01 * np.random.normal(loc=0, scale=1,
+			                                              size=(self.num_nodes[layer], self.num_nodes[layer + 1]))
 
 		# initialise weight-deltas
 		self.reset_weight_deltas()
@@ -48,7 +49,7 @@ class NeuralNet:
 		# initialize the outputs, deltas to empty
 		for output in range(len(self.outputs)):
 			self.outputs[output] = np.empty((num_nodes[output], 1))
-			# self.deltas[output] = np.empty((num_nodes[output], 1))
+		# self.deltas[output] = np.empty((num_nodes[output], 1))
 
 	def reset_weight_deltas(self):
 		for layer in range(self.num_layers - 1):
@@ -57,19 +58,19 @@ class NeuralNet:
 	def forward_phase(self, input):
 		input = input.reshape(-1, 1)
 		self.outputs[0] = input
-		self.outputs_derivative[0] = Relu.grad(input)
+		self.outputs_derivative[0] = Activation.grad(input, self.activation_function)
 		for layer in range(1, self.num_layers):
 			output = np.dot(np.transpose(self.weights[layer - 1]), self.outputs[layer - 1]) + self.biases[layer]
 			if layer == self.num_layers - 1:
 				output = Softmax.value(output)
 				self.outputs_derivative[-1] = Softmax.grad(output)
 			else:
-				output = Relu.value(output)
-				self.outputs_derivative[layer] = Relu.grad(output)
+				output = Activation.value(output, self.activation_function)
+				self.outputs_derivative[layer] = Activation.grad(output, self.activation_function)
 			self.outputs[layer] = output
 
 	def update_weights(self, batch_size):
-		for layer in range(self.num_layers -1):
+		for layer in range(self.num_layers - 1):
 			self.weights[layer] += self.weight_deltas[layer] / batch_size
 
 	def backward_phase(self, d, update_weights, batch_size):
@@ -78,15 +79,16 @@ class NeuralNet:
 		d = d.reshape(-1, 1)
 		# calculate deltas for the output layer
 		# self.deltas[-1] = np.multiply(self.outputs_derivative[-1], (d - self.outputs[-1]) / self.outputs[-1].shape[0])
-		self.deltas[-1] = (d - self.outputs[-1]) / 10 #* self.outputs_derivative[-1] #/ self.outputs[-1].shape[0] # TODO remove this
-		#calculate bias for output layer
+		self.deltas[-1] = (d - self.outputs[
+			-1]) / 10  # * self.outputs_derivative[-1] #/ self.outputs[-1].shape[0] # TODO remove this
+		# calculate bias for output layer
 		self.biases[-1] += self.learning_rate * self.deltas[-1]
 		# print(self.deltas[-1])
 		# print()
 		if update_weights:
 			self.update_weights(batch_size=batch_size)
 			self.reset_weight_deltas()
-			# self.weights -= self.weight_deltas / batch_size
+		# self.weights -= self.weight_deltas / batch_size
 
 		# calculate deltas for previous layers and update weights
 		for layer in range(self.num_layers - 2, -1, -1):
@@ -95,7 +97,7 @@ class NeuralNet:
 			self.weight_deltas[layer] += weight_delta
 			# self.weights[layer] -= weights_delta
 			self.biases[layer] += self.learning_rate * self.deltas[layer]
-			# self.biases[layer] += self.learning_rate * np.sum(self.deltas[layer],axis=0,keepdims=True) # TODO remove this
+		# self.biases[layer] += self.learning_rate * np.sum(self.deltas[layer],axis=0,keepdims=True) # TODO remove this
 
 	def fit(self, x, y, batch_size, epochs):
 		for epoch in range(epochs):
@@ -115,6 +117,7 @@ class NeuralNet:
 					update_weights = True
 					batch_iter = 0
 				self.backward_phase(d, update_weights=update_weights, batch_size=batch_size)
+			score_epoch /= x.shape[0]
 			print(score_epoch)
 
 	def predict(self, X):
@@ -130,14 +133,38 @@ class NeuralNet:
 			max_index = np.argmax(y_pred)
 			accuracy_sum += (max_index == y_test[row])
 		return accuracy_sum / y_test.shape[0]
-		# return self.cross_entropy_loss(y_pred, y_test)
+
+	# return self.cross_entropy_loss(y_pred, y_test)
 
 	def cross_entropy_loss(self, y_pred, y_act):
-		return -(np.sum(np.dot(np.transpose(y_act), np.log(y_pred))) + np.sum(np.dot(np.transpose(1.0 - y_act), np.log(1.0 - y_pred))))
+		return -(np.sum(np.dot(np.transpose(y_act), np.log(y_pred))) + np.sum(
+			np.dot(np.transpose(1.0 - y_act), np.log(1.0 - y_pred))))
 
 	def get_train_outputs(self):
 		return self.outputs[-1]
 
+class Activation:
+	@staticmethod
+	def value(x, activation_function):
+		if activation_function == 'relu':
+			return Relu.value(x)
+		elif activation_function == 'linear':
+			return Linear.value(x)
+		elif activation_function == 'tanh':
+			return Tanh.value(x)
+		elif activation_function == 'sigmoid':
+			return Sigmoid.value(x)
+
+	@staticmethod
+	def grad(x, activation_function):
+		if activation_function == 'relu':
+			return Relu.grad(x)
+		elif activation_function == 'linear':
+			return Linear.grad(x)
+		elif activation_function == 'tanh':
+			return Tanh.grad(x)
+		elif activation_function == 'sigmoid':
+			return Sigmoid.grad(x)
 
 class Relu:
 	@staticmethod
@@ -149,33 +176,34 @@ class Relu:
 		return (np.sign(x) + 1) // 2
 		print(x)
 
+
 class Sigmoid:
 	@staticmethod
 	def value(x):
 		return 1 / (1 + np.exp(-x))
 
 	@staticmethod
-	def grad(self, x):
+	def grad(x):
 		return np.multiply(Sigmoid.value(x), (1 - Sigmoid.value(x)))
 
 
 class Linear:
 	@staticmethod
-	def value(m, x, c):
+	def value(x, m=1, c=0):
 		return m * x + c
 
 	@staticmethod
-	def grad(m, x, c):
+	def grad(x, m=1, c=0):
 		return m
 
 
 class Tanh:
 	@staticmethod
-	def value(a, b, x):
+	def value(x, a=1, b=1):
 		return a * np.tanh(b * x)
 
 	@staticmethod
-	def grad(a, b, x):
+	def grad(x, a=1, b=1):
 		return a * b / np.square(np.cosh(b * x))
 
 
@@ -192,30 +220,22 @@ class Softmax:
 
 
 if __name__ == '__main__':
-
-	training_image_set = idx2numpy.convert_from_file('images/train-images.idx3-ubyte')
-	training_label_set = idx2numpy.convert_from_file('images/train-labels.idx1-ubyte')
-	test_image_set = idx2numpy.convert_from_file('images/t10k-images.idx3-ubyte')
-	test_label_set = idx2numpy.convert_from_file('images/t10k-labels.idx1-ubyte')
-
-	training_y = np.transpose(np.asmatrix(training_label_set))
-	test_y = np.transpose(np.asmatrix(test_label_set))
-
-	training_set = np.zeros((training_image_set.shape[0], training_image_set.shape[1] ** 2))
-	test_set = np.zeros((test_image_set.shape[0], test_image_set.shape[1] ** 2))
-
-	for i in range(training_image_set.shape[0]):
-		training_set[i, :] = training_image_set[i].flatten()
-
-	for i in range(test_image_set.shape[0]):
-		test_set[i, :] = test_image_set[i].flatten()
-
-	x = training_set[:100, :]
-	y = training_y[:100, :]
-
+	training_images = idx2numpy.convert_from_file('images/train-images.idx3-ubyte')
+	training_labels = idx2numpy.convert_from_file('images/train-labels.idx1-ubyte')
+	test_images = idx2numpy.convert_from_file('images/t10k-images.idx3-ubyte')
+	test_labels = idx2numpy.convert_from_file('images/t10k-labels.idx1-ubyte')
+	training_y = training_labels.reshape(-1, 1)
+	test_y = test_labels.reshape(-1, 1)
+	x_train = np.empty((training_images.shape[0], 784))
+	x_test = np.empty((test_images.shape[0], 784))
+	for i in range(0, training_images.shape[0]):
+		x_train[i, :] = training_images[i, :].flatten()
+	for i in range(test_images.shape[0]):
+		x_test[i, :] = test_images[i, :].flatten()
+	x = x_train[:1000, :]
+	y = training_y[:1000, :]
 	num_inputs = x.shape[1]
 	num_labels = 10
-
-	neuralNet = NeuralNet(5, [num_inputs, 256, 128, 64, num_labels], 'relu', 0.1, num_labels, num_inputs)
-	neuralNet.fit(x, y, batch_size=10, epochs=100)
+	neuralNet = NeuralNet(5, [num_inputs, 256, 128, 64, num_labels], 'linear', 0.1, num_labels, num_inputs)
+	neuralNet.fit(x, y, batch_size=100, epochs=100)
 	print(neuralNet.score(x, y))
