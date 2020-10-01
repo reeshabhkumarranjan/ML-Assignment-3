7ac7660740552921d74289aa0b0b3ec0be923b00
Vinayak <32881538+vinayak42@users.noreply.github.com>
2020-10-01 13:38:44 +0530

    Removed unnecessary code

--- Problem 2/cnn.py
+++ Problem 2/cnn.py
@@ -34,10 +34,6 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # self.conv1 = nn.Conv2d(1, 6, 5)
-        # self.pool = nn.MaxPool2d(2, 2)
-        # self.conv2 = nn.Conv2d(6, 16, 3)
-        # self.svm_x = np.empty((0, 72))
         self.svm_x = None
         self.svm_x_is_None = True
         self.conv1 = nn.Conv2d(1, 6, 4)
@@ -49,9 +45,6 @@ class Net(nn.Module):
         self.svm_test_x = None
         self.svm_test_x_is_None = True
         
-        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        # self.fc2 = nn.Linear(120, 84)
-        # self.fc3 = nn.Linear(84, 10)
         self.fc1 = nn.Linear(16 * 5 * 5, 256)
         self.fc2 = nn.Linear(256, 10)
 
@@ -60,12 +53,8 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
-        # x = x.view(x.size(0), -1)
-        # x = F.relu(self.fc1(x))
 
         x = self.fc1(x)
-        # print(x.detach().numpy().shape)
-        # x = x.view(x.size(0), -1)
         _x = x.cpu().detach().numpy()
 
         if self.create_svm_x:
@@ -82,14 +71,7 @@ class Net(nn.Module):
             else:
                 self.svm_test_x = np.concatenate((self.svm_test_x, _x), axis=0)
             
-        
-        # np.append(self.svm_x, _x, axis=0)
-        # print(self.svm_x.shape)
-        # print(_x.shape)
         x = F.relu(x)
-
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
         x = self.fc2(x)
         return x
 
@@ -111,8 +93,6 @@ for epoch in range(10):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        # inputs, labels = data
-        # images, labels = data.to(device), data.to(device)
         inputs, labels = data[0].to(device), data[1].to(device)
 
         # zero the parameter gradients
@@ -142,18 +122,14 @@ print('Finished Training')
 dataiter = iter(testloader)
 images, labels = dataiter.next()
 outputs = net(images)
-# import torch
 
 correct = 0
 total = 0
 net.create_svm_test_x = True
 with torch.no_grad():
     for data in testloader:
-        # images, labels = data
-        # images, labels = data.to(device), data.to(device)
+
         images, labels = data[0].to(device), data[1].to(device)
-        # images = images.cuda()
-        # labels = labels.cuda()
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         predicted = predicted.cpu()
@@ -168,6 +144,7 @@ import torch
 from sklearn.metrics import confusion_matrix
 trainloader1 = torch.utils.data.DataLoader(fashiontrain, batch_size=60000, shuffle=True, num_workers=2)
 testloader1 = torch.utils.data.DataLoader(fashiontest, batch_size=10000, shuffle=False, num_workers=2)
+
 with torch.no_grad():
     for data in testloader1:
         images, labels = data[0].to(device), data[1].to(device)
@@ -175,6 +152,7 @@ with torch.no_grad():
         _, predicted = torch.max(outputs.data, 1)
         predicted = predicted.cpu()
         cm_test = confusion_matrix(labels,predicted)
+        
 with torch.no_grad():
     for data in trainloader1:
         images, labels = data[0].to(device), data[1].to(device)
@@ -182,6 +160,7 @@ with torch.no_grad():
         _, predicted = torch.max(outputs.data, 1)
         predicted = predicted.cpu()
         cm_train = confusion_matrix(labels,predicted)
+        
 print(cm_train)
 print(cm_test)
 
@@ -191,7 +170,6 @@ svClassifier = svm.SVC(kernel='rbf', gamma='scale', verbose=True)
 svClassifier.fit(net.svm_x, svm_train_y.reshape(-1, ))
 
 # Generating test dataset for SVM
-# svClassifier.predict(net.svm_test_x)
 svClassifier.score(net.svm_test_x, svm_test_y.reshape(-1, ))
 
 svm_test_y.reshape(-1, )
